7e73bc61728a2595c90b79ac3a127ffd00f4b7ba
Shariq Shoaib <rajashariq435@gmail.com>
2024-08-21 17:50:06 +0500

    first

---
+++ src/Background.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./BackgroundVideo.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faInstagram, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons";
+
+const BackgroundVideo = () => {
+  return (
+    <div className="background-video-container">
+      <video autoPlay loop muted className="background-video">
+        <source src="/background1.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="content-overlay">
+        <h1 className="name-title">Muhammad Sahriq Shoaib</h1>
+        <h2 className="title">Web Developer</h2>
+        <div className="social-links">
+          <a href="https://github.com/your-github-username" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} /> GitHub
+          </a>
+          <a href="https://www.instagram.com/your-instagram-username" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faInstagram} /> Instagram
+          </a>
+          <a href="https://www.linkedin.com/in/your-linkedin-username" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+          </a>
+          <a href="https://twitter.com/your-twitter-username" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faTwitter} /> Twitter
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BackgroundVideo;
